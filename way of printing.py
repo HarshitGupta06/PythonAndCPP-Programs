98cac55d2260edd839f1ecaf2fb15f50293a305a
Deepanshu Mishra <deepanshusuper16@gmail.com>
2019-10-23 12:04:23 +0530

    here you will learn how print statement executes in python

---
+++ way of printing.py
@@ -0,0 +1,13 @@
+print("put here whatever you want to print") #this is the way how you can comment
+
+print("5") #you can print number within th "" or without them
+
+print(5)#like this
+
+print("5+5")#this will be considerd as string
+
+print(5+5)#whereas this will be considerd as mathematical problem
+
+print("name ",end = "")#use this to stay in the same line
+
+print("surname")#this will add the surname in the above line
