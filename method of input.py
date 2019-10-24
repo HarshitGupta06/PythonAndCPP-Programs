6ea74c7a66083f5e95272b67019789858ecfdb9c
Deepanshu Mishra <deepanshusuper16@gmail.com>
2019-10-24 08:22:55 +0530

    Python-file added

---
+++ method of input.py
@@ -0,0 +1,3 @@
+#Here we will learn how to take input from user in a specified format
+a = int(input("enter something"))#int at the starting shows that we have typecasted the input into integer type
+print(a)#this is how a variable is printed
